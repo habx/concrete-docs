098964a1b9e85f9a42516bfcd5ef1e4275c64eab
Jean Dessane <jean@habx.fr>
2021-03-25 22:32:06 +0100

    fix

--- src/pages/Home.style.ts
+++ src/pages/Home.style.ts
@@ -5,6 +5,7 @@ import { Background } from '@habx/ui-core'
 export const HomeImg = styled.img`
   position: absolute;
   z-index: -1;
+  width: 100%;
 `
 
 export const HomeMainContainer = styled(Background).attrs(() => ({
