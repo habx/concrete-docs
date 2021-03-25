b284d6ed0076a6683519da52317183a7b01f4a6c
Jean Dessane <jean@habx.fr>
2021-03-25 20:34:29 +0100

    Use root theme

---
+++ src/theme/Root.js
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+import { Provider, ThemeProvider, theme } from '@habx/ui-core'
+
+const GlobalStyle = createGlobalStyle`
+  
+  :root {
+    --ifm-color-primary: ${theme.color('primary')};
+    --ifm-color-primary-dark: ${theme.color('primary')};
+    --ifm-color-primary-darker: ${theme.color('primary', {
+      variation: 'loud',
+    })};
+    --ifm-color-primary-darkest: ${theme.color('primary', {
+      variation: 'louder',
+    })};
+    --ifm-color-primary-light: ${theme.color('primary', { variation: 'calm' })};
+    --ifm-color-primary-lighter: ${theme.color('primary', {
+      variation: 'calmer',
+    })};
+    --ifm-color-primary-lightest: ${theme.color('primary', {
+      variation: 'calmer',
+    })};
+    --ifm-code-font-size: 95%;
+  }
+
+  .docusaurus-highlight-code-line {
+    background-color: rgb(72, 77, 91);
+    display: block;
+    margin: 0 calc(-1 * var(--ifm-pre-padding));
+    padding: 0 var(--ifm-pre-padding);
+  }
+  #__docusaurus , #__docusaurus > div {
+    min-height: 0;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export default ({ children }) => {
+  return (
+    <Provider>
+      <ThemeProvider>
+        {children}
+        <GlobalStyle />
+      </ThemeProvider>
+    </Provider>
+  )
+}
