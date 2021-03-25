3fc899126b5dd9ea21769cf3a3221b22f13616c4
Jean Dessane <jean@habx.fr>
2021-03-25 22:34:27 +0100

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,24 @@ import useBaseUrl from '@docusaurus/useBaseUrl'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import * as React from 'react'
+import styled from 'styled-components'
 
-import { Button, Title } from '@habx/ui-core'
+import { Background, Button, Title } from '@habx/ui-core'
 
-import { HomeImg, HomeMainContainer } from './Home.style'
+export const HomeImg = styled.img`
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+`
+
+export const HomeMainContainer = styled(Background).attrs(() => ({
+  backgroundColor: 'black',
+}))`
+  display: grid;
+  grid-gap: 32px;
+  margin: 128px;
+  background: transparent !important;
+`
 
 export default function Home() {
   const context = useDocusaurusContext()
